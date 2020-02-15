7609bd700807cd301f1e2cf477e7082cfedc48ce
Adam Bujak <adamjbujak@gmail.com>
2020-02-15 11:23:07 -0500

    Added emacs config file

---
+++ init.el
@@ -0,0 +1,44 @@
+
+(setq inhibit-startup-screen t)
+
+(if (display-graphic-p)
+    (setq initial-frame-alist
+          '(
+            (tool-bar-lines . 0)
+            (width . 106)
+            (height . 60)
+            ;; (left . 50)
+            ;; (top . 50)
+            ))
+  (setq initial-frame-alist '( (tool-bar-lines . 0))))
+
+(setq default-frame-alist
+      '(
+        (tool-bar-lines . 0)
+
+        (width . 100)
+        (height . 50)))
+(show-paren-mode 1)
+(menu-bar-mode -1)
+(toggle-scroll-bar -1)
+(tool-bar-mode -1)
+
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+(package-refresh-contents)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (rust-mode))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(add-hook 'rust-mode-hook
+          (lambda () (setq indent-tabs-mode nil)))

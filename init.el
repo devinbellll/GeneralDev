4b523e42695baafe3efdb6a7374e2b6945457df7
adambujak <adamjbujak@gmail.com>
2020-02-21 00:26:10 -0500

    Added split window feature

--- init.el
+++ init.el
@@ -1,6 +1,8 @@
 
 (setq inhibit-startup-screen t)
-
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
+(load-theme 'dracula t)
+(split-window-right)
 (if (display-graphic-p)
     (setq initial-frame-alist
           '(
@@ -23,11 +25,7 @@
 (toggle-scroll-bar -1)
 (tool-bar-mode -1)
 
-(require 'package)
-(add-to-list 'package-archives
-             '("melpa" . "https://melpa.org/packages/") t)
-(package-initialize)
-(package-refresh-contents)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.

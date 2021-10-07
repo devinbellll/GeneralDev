5021a68f809017148658c5fb279a2098bc3240b4
Adam Bujak <adamjbujak@gmail.com>
2021-10-06 21:04:14 -0400

    fix ohmyzsh install

--- mac/install_oh_my_zsh.sh
+++ mac/install_oh_my_zsh.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 git submodule update --init --recursive
-cp -R .oh-my-zsh ~
+rm -rf ~/.oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/adambujak/ohmyzsh/master/tools/install.sh)"
+rm ~/.zshrc
 cp .zshrc ~

3cd2799371afbc3793d0341b96b58b138d9d7f17
Adam Bujak <adamjbujak@gmail.com>
2021-12-07 16:56:32 -0500

    add light airline theme

--- mac/install_vim.sh
+++ mac/install_vim.sh
@@ -8,4 +8,5 @@ cp light_vimrc ~/.vimrc
 
 git clone git://github.com/ajh17/VimCompletesMe.git ~/.vim/pack/vendor/start/VimCompletesMe
 git clone https://github.com/qpkorr/vim-renamer ~/.vim/bundle/vim-renamer
-
+git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
+git clone https://github.com/vim-airline/vim-airline-themes.git ~/.vim/bundle/vim-airline-themes

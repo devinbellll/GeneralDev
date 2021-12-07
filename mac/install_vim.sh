2170d0c5d4ab02495bf4fb3e93c29e7da7ad072c
Adam Bujak <adamjbujak@gmail.com>
2021-12-07 14:36:46 -0500

    more light theme work

--- mac/install_vim.sh
+++ mac/install_vim.sh
@@ -4,7 +4,7 @@ rm -rf ~/.vim ~/.vimrc
 
 git submodule update --init --recursive
 cp -R .vim ~
-cp .vimrc ~
+cp light_vimrc ~/.vimrc
 
 git clone git://github.com/ajh17/VimCompletesMe.git ~/.vim/pack/vendor/start/VimCompletesMe
 git clone https://github.com/qpkorr/vim-renamer ~/.vim/bundle/vim-renamer

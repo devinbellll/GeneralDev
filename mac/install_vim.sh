6f56007b96e29234a7a2396314c299191facab4b
Adam Bujak <adamjbujak@gmail.com>
2022-03-26 21:10:10 -0400

    add gutentags

--- mac/install_vim.sh
+++ mac/install_vim.sh
@@ -8,6 +8,7 @@ cp light_vimrc ~/.vimrc
 
 git clone https://github.com/junegunn/fzf ~/.vim/bundle/fzf
 git clone https://github.com/ajh17/VimCompletesMe.git ~/.vim/pack/vendor/start/VimCompletesMe
+git clone https://github.com/ludovicchabant/vim-gutentags.git ~/.vim/bundle/vim-gutentags
 git clone https://github.com/qpkorr/vim-renamer ~/.vim/bundle/vim-renamer
 git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
 git clone https://github.com/vim-airline/vim-airline-themes.git ~/.vim/bundle/vim-airline-themes

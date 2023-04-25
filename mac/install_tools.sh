6d01edd55f1493795c177ee96c36ffc087ce667f
Adam Bujak <a_bujak@apple.com>
2023-04-25 14:35:10 -0700

    Add git config setting

--- mac/install_tools.sh
+++ mac/install_tools.sh
@@ -2,3 +2,12 @@ brew install --HEAD universal-ctags/universal-ctags/universal-ctags
 brew install fzf
 brew install the_silver_searcher
 python3 -m venv ~/venv
+
+pushd ~ > /dev/null
+HOME_PATH=$(pwd)
+HOME_PATH=$(echo $HOME_PATH | sed 's/\//\\\//g')
+popd > /dev/null
+
+cp global_gitignore ~/.gitignore
+sed "s/HOME_DIRECTORY/${HOME_PATH}/g" "global_gitconfig" > ~/.gitconfig
+

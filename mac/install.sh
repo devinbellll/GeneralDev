ff1b41e19bf0650d8dceb783402bacc119d71bbf
Adam Bujak <a_bujak@apple.com>
2023-07-12 11:27:33 -0700

    abstract theme from neovimrc

--- mac/install.sh
+++ mac/install.sh
@@ -2,7 +2,7 @@
 
 ./install_oh_my_zsh.sh
 ./install_zshrc.sh
-./install_neovim.sh
+./install_neovim.sh light
 ./install_tmux.sh
 ./install_zprofile.sh
 ./install_tools.sh
--- mac/install_neovim.sh
+++ mac/install_neovim.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+DEFAULT_THEME="light"
+THEME="${1:-$DEFAULT_THEME}"
+
+pushd ~ > /dev/null
+HOME_PATH=$(pwd)
+popd > /dev/null
+
 brew install nvim 
 pip3 install pynvim
 
@@ -9,11 +16,21 @@ git submodule update --init --recursive
 
 mkdir -p ~/.config/nvim
 mkdir -p ~/.nvim
-cp neovimrc_light ~/.config/nvim/init.vim
-cp neovimrc_dark ~/.config/nvim/init_dark.vim
+
+cp neovimrc_dark ~/.config/nvim/dark_nvim_theme.vim
+cp neovimrc_light ~/.config/nvim/light_nvim_theme.vim
+
+
+if [ $THEME == "dark" ]; then
+  THEME_PATH=$(echo "~/.config/nvim/dark_nvim_theme.vim" | sed 's/\//\\\//g')
+else 
+  THEME_PATH=$(echo "~/.config/nvim/light_nvim_theme.vim" | sed 's/\//\\\//g')
+fi
+
+# copy neovimrc to proper place and set correct theme file
+sed "s/MY_VIM_THEME_SOURCE/${THEME_PATH}/g" "neovimrc" > $HOME_PATH/.config/nvim/init.vim
 
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
             https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.nvim/bundle/Vundle
-

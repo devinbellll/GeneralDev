a82fb47626d94c9eae7c79185611c20183b9a15d
Adam Bujak <a_bujak@apple.com>
2024-02-13 14:24:37 -0800

    remove dependency on forked oh my zsh repo

--- mac/install_oh_my_zsh.sh
+++ mac/install_oh_my_zsh.sh
@@ -3,4 +3,8 @@
 git submodule update --init --recursive
 rm -rf ~/.oh-my-zsh
 rm ~/.zshrc
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/adambujak/ohmyzsh/master/tools/install.sh)"
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+cp ./half-life-mod-light.zsh-theme ~/.oh-my-zsh/themes/
+cp ./half-life-mod-dark.zsh-theme ~/.oh-my-zsh/themes/

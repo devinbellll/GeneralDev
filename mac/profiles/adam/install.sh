146f8dd39c651a11e5c23a2c6b53d530db248591
Adam Bujak <abujak@amazon.com>
2025-04-14 15:34:10 -0400

    remove old theme stuff and fix tmux highlighting

--- mac/profiles/adam/install.sh
+++ mac/profiles/adam/install.sh
@@ -5,14 +5,5 @@ current_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 pushd "$current_path"
 
 # copy ohmyzsh themes
-cp themes/half-life-mod-dark.zsh-theme ~/.oh-my-zsh/themes
-cp themes/half-life-mod-light.zsh-theme ~/.oh-my-zsh/themes
+cp themes/half-life-rose-pine.zsh-theme ~/.oh-my-zsh/themes
 
-# copy nvim themes
-cp themes/neovimrc_dark ~/.config/nvim/dark_nvim_theme.vim
-cp themes/neovimrc_light ~/.config/nvim/light_nvim_theme.vim
-cp -R lua ~/.config/nvim/
-
-# append tmux theme to existing tmux
-cat themes/tmux_theme >> ~/.tmux.conf
-tmux source-file ~/.tmux.conf
